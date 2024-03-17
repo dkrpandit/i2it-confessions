eec10c4c942a891cfd60de24c65184e74fc7b78f
Dharmendra Pandit <dharmendra763297@gmail.com>
2024-03-17 12:02:45 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 const connection = require("./utils/db");
 const router = require("./router/auth-router");
 
-const errorMiddleware = require("./middleware/error-middleware")
+const errorMiddleware = require("./middleware/error-middleware");
+
+// handling cors polices
+const corsOptions = {
+    origin: "https://i2it-confessions-6dhi.vercel.app",
+    methods: "GET,POST,DELETE,PUT,PATCH,HEAD",
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
 const PORT = process.env.PORT || 5000;
 
-// handling cors polices
-const corsOptions = {
-    origin: "https://i2it-confessions-6dhi.vercel.app",
-    methods: "GET,POST,DELETE,PUT,PATCH,HEAD",
-    Credential: true
-}
-app.use(cors(corsOptions))
-
 connection().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
-})
+});
